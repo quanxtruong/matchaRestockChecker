de3942d9ee51fb6776c1aa60438a1f9b6bd8581e
quanxtruong <cunqtran@gmail.com>
2025-03-18 00:46:49 -0500

    ajsdfhakl

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def process_stock(url, stock_dict):
     data = response.json()
     for product in data["products"]:
         product_title = product["title"]
-        if product_title == "Digital Gift Card" or product_title == "Matcha To-Go Packets":
+        if product_title == "Digital Gift Card" or product_title == "Matcha To-Go Packets - 10 x 2g":
             continue
         product_url = f"{url}/{product['handle']}"
         variant = product["variants"][0]
